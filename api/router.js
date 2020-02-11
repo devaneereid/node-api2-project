7df7c9daf9a5670363a22a455e165647e8022ddd
Devanee Reid <devaneereid@gmail.com>
2020-02-11 16:55:51 -0700

    updated post request

--- api/router.js
+++ api/router.js
@@ -58,22 +58,47 @@ router.get("/:id/comments", (req, res) => {
 
 // POST Request
 router.post('/', (req, res) => {
-    Data.insert(req.body)
-        .then(info => {
-            if(info){
-                res.status(201).json(info);
-            } else {
-                res.status(404).json({
-                    message: "The post with the specified ID does not exist.",
+    const {title, contents} = req.body;
+        if(!title && !contents){
+            res.status(400).json({ errorMessage: "Please provide title and content"})
+        } else {
+            Data.insert({title, contents})
+                .then(info => {
+                    res.status(201).json(info)
                 })
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(500).json({
-                error: "There was an error while saving the post to the database",
-            });
-        });
+                .catch(error => {
+                console.log(error);
+                res.status(500).json({
+                    error: "There was an error while saving the post to the database",
+                });
+            })
+        }
+});
+
+router.post("/:id/comments", (req, res) => {
+    const newComment = req.body;
+        if(newComment) {
+            Data.insertComment(req.body)
+                .then(comment => {
+                    if (comment) {
+                        res.status(201).json(comment)
+                    } else {
+                        res.status(404).json({
+                            message: "The post with the specified ID does not exist."
+                        })
+                    }
+                })
+                .catch(error => {
+                    console.log('Error posting comment', error)
+                    res.status(500).json({
+                        error: "There was an error while saving the post to the database",
+                    });
+                })
+                } else {
+                    res.status(400).json({
+                        errorMessage: "Please provide title and contents for the post."
+                    })
+                }
 });
 
 // DELETE Request
@@ -102,9 +127,9 @@ router.delete("/:id", (req, res) => {
 router.put('/:id', (req, res) => {
     const changes = req.body;
     Data.update(req.params.id, changes)
-        .then(data => {
-            if (data) {
-                res.status(200).json(data);
+        .then(post => {
+            if (post) {
+                res.status(200).json(post);
             } else {
                 res.status(404).json({
                     message: "The post with the specified ID does not exist.",
@@ -118,6 +143,7 @@ router.put('/:id', (req, res) => {
             });
         });
 });
+
 
 module.exports = router;
 
